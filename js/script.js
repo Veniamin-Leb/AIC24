e45783cde61b5193ec75ef9a85305c09cf9910ec
Veniamin-Leb <62715894+Veniamin-Leb@users.noreply.github.com>
2021-06-28 21:11:15 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-import ('../bootstrap/dist/js/bootstrap.js')
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+$(document).ready(function(){
+    $(":input").inputmask();
+    
+    $("#exampleInputPassword2").inputmask({
+    mask: '+7 (999) 999-99-99',
+    placeholder: '+7 (___) ___-__-__',
+    showMaskOnHover: false,
+    showMaskOnFocus: false,
+    onBeforePaste: function (pastedValue, opts) {
+    var processedValue = pastedValue;
+    
+    //do something with it
+    
+    return processedValue;
+    }
+    });
+    });
