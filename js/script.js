d20e00aa94250467987d743970b5d1ea9a6795cc
Veniamin-Leb <62715894+Veniamin-Leb@users.noreply.github.com>
2021-06-29 17:50:59 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,10 +5,18 @@ var sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
+    document.getElementById("myBtn").style.display = "block";
   } else {
     navbar.classList.remove("sticky");
+    document.getElementById("myBtn").style.display = "none";
   }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 $(document).ready(function(){
@@ -28,3 +36,5 @@ $(document).ready(function(){
     }
     });
     });
+
+
