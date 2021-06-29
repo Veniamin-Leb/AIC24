ad593cb32678755299799263f13753a6a6ddd4c4
Veniamin-Leb <62715894+Veniamin-Leb@users.noreply.github.com>
2021-06-29 19:14:29 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
-window.onscroll = function() {myFunction()};
+let nav = $("#navbar")
+let main = $("#main");
+let mainH;
+let scrollPos = $(window).scrollTop();
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+$(window).on("scroll load resize", function(){
+  mainH = main.innerHeight();
+  scrollPos = $(this).scrollTop();
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
+
+  if (scrollPos > mainH) {
     navbar.classList.add("sticky");
     document.getElementById("myBtn").style.display = "block";
   } else {
     navbar.classList.remove("sticky");
     document.getElementById("myBtn").style.display = "none";
   }
+});
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+$("[data-scroll]").on("click", function(event){
+  event.preventDefault();
+
+  let elemID = $(this).data('scroll');
+  let elemOffset = $(elemID).offset().top;
+
+  $("html, body").animate({
+    scrollTop: elemOffset - 40
+  }, 100);
+});
+
 
 $(document).ready(function(){
     $(":input").inputmask();
@@ -36,5 +51,3 @@ $(document).ready(function(){
     }
     });
     });
-
-
